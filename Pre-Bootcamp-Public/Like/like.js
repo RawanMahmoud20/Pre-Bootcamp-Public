5c9abbc8a120635912ab81db250a987959db4a41
Rawan Mahmoud <rawanmahmoud323@gmail.com>
2026-03-26 13:43:52 +0200

    like button

--- Pre-Bootcamp-Public/Like/like.js
+++ Pre-Bootcamp-Public/Like/like.js
@@ -1,10 +1,11 @@
+// Array to store the like counts for each post
 let likesArray = [9, 12, 9];
 
 function addLike(index) {
-
+// Increment the value in the array for the clicked post
     likesArray[index]+=1;
 
 let allCounters = document.querySelectorAll(".likes-counter");
-    // تحديث الرقم في الصفحة
-    allCounters[index].innerText = likesArray[index];
+// Update the text of the specific counter element with the new value   
+allCounters[index].innerText = likesArray[index];
 }
