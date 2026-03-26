c132541d31da274d3e61c4fc4b3c6a7248dfaeae
Rawan Mahmoud <rawanmahmoud323@gmail.com>
2026-03-26 13:40:41 +0200

    like button

--- Pre-Bootcamp-Public/Like/like.js
+++ Pre-Bootcamp-Public/Like/like.js
@@ -1,20 +1,10 @@
+let likesArray = [9, 12, 9];
 
+function addLike(index) {
 
-// choose elements from page
-const likeButton = document.querySelector("#like-btn");
-const likeCountElement = document.querySelector("#like-count");
+    likesArray[index]+=1;
 
-// initialize the number of likes
-let likeCount = 0;
-likeCountElement.innerText = likeCount;
-
-likeButton.onclick = function() {
-    // extract the current number and convert it to an integer
-    let currentLikes = parseInt(likeCountElement.innerText);
-
-    // increment the number by 1
-    currentLikes++;
-
-    // update the text inside the element with the new number
-    likeCountElement.innerText = currentLikes;
-};
+let allCounters = document.querySelectorAll(".likes-counter");
+    // تحديث الرقم في الصفحة
+    allCounters[index].innerText = likesArray[index];
+}
