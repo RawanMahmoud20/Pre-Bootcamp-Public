0647ee2ab4067f54aa7c2fe22790c7d9f2eac0ae
Rawan Mahmoud <rawanmahmoud323@gmail.com>
2026-04-12 16:47:10 +0300

    hello world

---
+++ python_stack/python_/python_fundamentals/hello_world.py
@@ -0,0 +1,16 @@
+print("Hello World")
+name= "Rawan Mahmoud"
+print("Hello",name)
+print("Hello " + name)
+age=23
+print("My age is : ", age)
+
+print("I'm {} years old".format(age))
+name = 42
+print("Hello", name)
+print("Hello " + str(name) )
+# 4. print "I love to eat sushi and pizza." with the foods in variables
+fav_food1= "pizza"
+fav_food2= "sushi"
+print("I love to eat {} and {}.".format(fav_food1, fav_food2))
+print(f"I love to eat {fav_food1} and {fav_food2}.")
--- python_stack/python_/python_fundamentals/test.py
+++ python_stack/python_/python_fundamentals/test.py
@@ -1,1 +1,24 @@
 print("Python is working!")
+x= "Hello Python"
+y =42 
+print (x , " The Number is : "+  str(y))
+first_name= "Rawan"
+last_name= "Mahmoud"
+age = 23
+
+print (f"My name is {first_name} {last_name} and I'm {age} years old.")
+print ("Another way to print string " + "My name is {} {} and I'm {} years old.".format(first_name , last_name , age ))
+# % format to you reserve 
+hl ="Hello %s" % "world"
+py = "I love python %d" % 3
+print (hl , py)
+print (f"My name is  %s %s  and I'm %d years old." % (first_name , last_name , age))
+print(py.title())
+print(py.capitalize())
+print(py.count("o"))
+print(py.upper())
+print(py.lower())
+print(py.split(""))
+print(py.find("python"))
+print(py.isalnum())
+print(py.endswith("3"))
