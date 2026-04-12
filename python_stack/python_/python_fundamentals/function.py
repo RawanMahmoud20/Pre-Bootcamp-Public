823d15b74cacc628753abddd71c82ba9831bd605
Rawan Mahmoud <rawanmahmoud323@gmail.com>
2026-04-12 19:48:48 +0300

    function

---
+++ python_stack/python_/python_fundamentals/function.py
@@ -0,0 +1,120 @@
+# --- Functions Basic I ---
+
+#1
+def a1():
+    return 5
+print(a1()) # Output: 5
+
+#2
+def a2():
+    return 5
+print(a2()+a2()) # Output: 10
+
+#3
+def a3():
+    return 5
+    return 10
+print(a3()) # Output: 5
+
+#4
+def a4():
+    return 5
+    print(10)
+print(a4()) # Output: 5
+
+#5
+def a5():
+    print(5)
+x = a5()
+print(x) # Output: 5, None
+
+#6
+def a6(b,c):
+    print(b+c)
+# print(a6(1,2) + a6(2,3))  # (None)
+
+#7
+def a7(b,c):
+    return str(b)+str(c)
+print(a7(2,5)) # Output: 25
+
+#8
+def a8():
+    b = 100
+    print(b)
+    if b < 10:
+        return 5
+    else:
+        return 10
+    return 7
+print(a8()) # Output: 100, 10
+
+#9
+def a9(b,c):
+    if b<c:
+        return 7
+    else:
+        return 14
+    return 3
+print(a9(2,3))    # Output: 7
+print(a9(5,3))    # Output: 14
+print(a9(2,3) + a9(5,3)) # Output: 21
+
+#10
+def a10(b,c):
+    return b+c
+    return 10
+print(a10(3,5)) # Output: 8
+
+#11
+b = 500
+print(b)
+def a11():
+    b = 300
+    print(b)
+print(b)
+a11()
+print(b) # Output: 500, 500, 300, 500
+
+#12
+b = 500
+print(b)
+def a12():
+    b = 300
+    print(b)
+    return b
+print(b)
+a12()
+print(b) # Output: 500, 500, 300, 500
+
+#13
+b = 500
+print(b)
+def a13():
+    b = 300
+    print(b)
+    return b
+print(b)
+b = a13()
+print(b) # Output: 500, 500, 300, 300
+
+#14
+def a14():
+    print(1)
+    b14()
+    print(2)
+def b14():
+    print(3)
+a14() # Output: 1, 3, 2
+
+#15
+def a15():
+    print(1)
+    x = b15()
+    print(x)
+    return 10
+def b15():
+    print(3)
+    return 5
+y = a15()
+print(y) # Output: 1, 3, 5, 10
--- python_stack/python_/python_fundamentals/test.py
+++ python_stack/python_/python_fundamentals/test.py
@@ -1,24 +1,112 @@
-print("Python is working!")
-x= "Hello Python"
-y =42 
-print (x , " The Number is : "+  str(y))
-first_name= "Rawan"
-last_name= "Mahmoud"
-age = 23
+# print("Python is working!")
+# x= "Hello Python"
+# y =42 
+# print (x , " The Number is : "+  str(y))
+# first_name= "Rawan"
+# last_name= "Mahmoud"
+# age = 23
 
-print (f"My name is {first_name} {last_name} and I'm {age} years old.")
-print ("Another way to print string " + "My name is {} {} and I'm {} years old.".format(first_name , last_name , age ))
-# % format to you reserve 
-hl ="Hello %s" % "world"
-py = "I love python %d" % 3
-print (hl , py)
-print (f"My name is  %s %s  and I'm %d years old." % (first_name , last_name , age))
-print(py.title())
-print(py.capitalize())
-print(py.count("o"))
-print(py.upper())
-print(py.lower())
-print(py.split(""))
-print(py.find("python"))
-print(py.isalnum())
-print(py.endswith("3"))
+# print (f"My name is {first_name} {last_name} and I'm {age} years old.")
+# print ("Another way to print string " + "My name is {} {} and I'm {} years old.".format(first_name , last_name , age ))
+# # % format to you reserve 
+# hl ="Hello %s" % "world"
+# py = "I love python %d" % 3
+# print (hl , py)
+# print (f"My name is  %s %s  and I'm %d years old." % (first_name , last_name , age))
+# print(py.title())
+# print(py.capitalize())
+# print(py.count("o"))
+# print(py.upper())
+# print(py.lower())
+# print(py.split(""))
+# print(py.find("python"))
+# print(py.isalnum())
+# print(py.endswith("3"))
+
+
+# premative data type
+
+# is_hungry = True 
+# # composite types >> Tuples 
+# dog = ('Bruce', 'cocker' , 19 , False)
+# if is_hungry : 
+#     print(dog[0]) 
+# # mutable data >> list 
+# empty_list = []
+ninja= ['R', 'B', 'C', 'A']
+# empty_list.append(ninja)
+# print(empty_list)
+# # Dictionaries
+# empty_dict = {}
+new_person = {'name': 'John',
+              'age': 38, 
+              'weight': 160.2, 
+              'has_glasses': False
+            }
+# new_person['name'] = 'Jack'
+# new_person['hobbies'] = ['climbing', 'coding']
+# print(new_person)
+# w = new_person.pop('weight')
+# print(w)
+# print(new_person)
+
+# print (type (w))
+# print (type (new_person))
+# print (len (new_person))
+# for elem in new_person:
+#     print (elem)
+#     # range(start, stop, step)
+# for x in range(10):
+#     print(x)     
+# for x in range(0, 11, 2):
+#     print(x)
+
+# for x in range(11, 0, -2):
+#     print(x)    
+# for i in range(0,len(ninja)):
+#     print (i , ninja[i])    
+
+# for key , value in new_person.items() :
+#     print( key , ":" , value)  
+
+  
+# keys = list(new_person.keys())
+# for i in range(len(keys)):
+#     print(i, new_person[keys[i]])    
+# 0 John
+# 1 38
+# 2 160.2
+# 3 False
+
+# for i in new_person.keys():
+#     print(i)    
+# name
+# age
+# weight
+# has_glasses
+
+# for i in new_person.values():
+#     print(i)    
+
+    #   while loop
+# count= 0 
+# while count > 5 :
+#     print("looping - " , count)
+#     count+=1     
+# else:
+#     print("Final else")    
+ 
+# val = "string"
+# x = len(val)
+# i=0
+# while i < x :
+#     print("looping - " , val[i])
+#     if  val[i] == "i":
+#         break 
+#     i+=1   
+# else:
+#     print("Final else")     
+# def a( a , b):
+#     return print(a+b) 
+    
+# print (a(2,3))  
