5fc03e04996ca0b9db228232a7aa7b0af2a9784a
Rawan Mahmoud <rawanmahmoud323@gmail.com>
2026-04-15 15:30:23 +0300

    ASS insertionSort

--- python_stack/python_/python_fundamentals/selectionSort.py
+++ python_stack/python_/python_fundamentals/selectionSort.py
@@ -16,3 +16,25 @@ num= [55,64,67,78,20,23,34,80,10,88,99]
 print(f"befor sort : {num}") 
 selectionSort(num)
 print(f"after sort : {num}") 
+# insertion sort 
+
+def insertionSort(arr):
+  
+    for i in range(1, len(arr)):
+        key = arr[i]
+        
+        j = i - 1
+        
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+            
+        arr[j + 1] = key
+        
+    return arr
+
+
+data = [7,4,6,5]
+print(f"befor : {data}")
+insertionSort(data)
+print(f"after: {data}")
