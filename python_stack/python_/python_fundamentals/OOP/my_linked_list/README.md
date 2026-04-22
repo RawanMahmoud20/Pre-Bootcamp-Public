b121a1d01bfcd8b46eea622662c0460c1c439d89
Rawan Mahmoud <rawanmahmoud323@gmail.com>
2026-04-22 11:53:18 +0300

    Initial commit with structured classes

---
+++ python_stack/python_/python_fundamentals/OOP/my_linked_list/README.md
@@ -0,0 +1,14 @@
+# Singly Linked List Implementation
+
+This project is a basic implementation of the Singly Linked List data structure using Python.
+
+## Features
+- add_to_front: Adds an element to the beginning of the list.
+- add_to_back: Adds an element to the end of the list.
+- print_values: Prints all elements in the list.
+
+## How to Use
+
+You can use method chaining to easily add elements:
+my_list = SList()
+my_list.add_to_front("A").add_to_back("B").print_values()
---
+++ python_stack/python_/python_fundamentals/OOP/my_linked_list/link_list.py
@@ -0,0 +1,49 @@
+class SLNode:
+    """Represents a single node in a singly linked list."""
+    def __init__(self, val):
+        self.value = val
+        self.next = None
+
+
+class SList:
+    """Represents a Singly Linked List."""
+    def __init__(self):
+        self.head = None
+
+    def add_to_front(self, val):
+        """Adds a new node to the front of the list."""
+        new_node = SLNode(val)
+        current_head = self.head
+        new_node.next = current_head
+        self.head = new_node
+        return self  # Allows method chaining
+
+    def add_to_back(self, val):
+        """Adds a new node to the end of the list."""
+        if self.head is None:
+            self.add_to_front(val)
+            return self
+        
+        new_node = SLNode(val)
+        runner = self.head
+        while runner.next is not None:
+            runner = runner.next
+        runner.next = new_node
+        return self
+
+    def print_values(self):
+        """Prints all node values in the list."""
+        runner = self.head
+        while runner is not None:
+            print(runner.value)
+            runner = runner.next
+        return self
+
+
+# Test the code
+if __name__ == "__main__":
+    my_list = SList()
+    my_list.add_to_front("are") \
+           .add_to_front("Linked lists") \
+           .add_to_back("fun!") \
+           .print_values()
