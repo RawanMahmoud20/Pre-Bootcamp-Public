1db391770aaecb53a25938904978f8065bb428ba
Rawan Mahmoud <rawanmahmoud323@gmail.com>
2026-06-13 14:52:14 +0300

    tv-project

---
+++ python_stack/my_environments/tv_project/shows_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.root),                            
+    path('shows', views.index),                     
+    path('shows/new', views.new),                   
+    path('shows/create', views.create),              
+    path('shows/<int:id>', views.show_detail),        
+    path('shows/<int:id>/edit', views.edit),          
+    path('shows/<int:id>/update', views.update),      
+    path('shows/<int:id>/destroy', views.destroy),   
+]
---
+++ python_stack/my_environments/tv_project/shows_app/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Show
+
+
+def root(request):
+    return redirect('/shows')
+
+
+def index(request):
+    context = {
+        'shows': Show.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+
+def new(request):
+    return render(request, 'new.html')
+
+
+def create(request):
+    if request.method == 'POST':
+        new_show = Show.objects.create(
+            title=request.POST['title'],
+            network=request.POST['network'],
+            release_date=request.POST['release_date'],
+            description=request.POST['description']
+        )
+        return redirect(f'/shows/{new_show.id}')
+    return redirect('/shows/new')
+
+
+def show_detail(request, id):
+    context = {
+        'show': get_object_or_404(Show, id=id)
+    }
+    return render(request, 'show_detail.html', context)
+
+
+def edit(request, id):
+    context = {
+        'show': get_object_or_404(Show, id=id)
+    }
+    return render(request, 'edit.html', context)
+
+def update(request, id):
+    if request.method == 'POST':
+        show = get_object_or_404(Show, id=id)
+        show.title = request.POST['title']
+        show.network = request.POST['network']
+        show.release_date = request.POST['release_date']
+        show.description = request.POST['description']
+        show.save()
+        return redirect(f'/shows/{show.id}')
+    return redirect(f'/shows/{id}/edit')
+
+def destroy(request, id):
+    if request.method == 'POST': 
+    
+        show = get_object_or_404(Show, id=id)
+        show.delete()
+    return redirect('/shows')
